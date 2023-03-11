38cd103524175586a73dd9e4958fbc670904770a
rianyrzq <127088643+rianyrzq@users.noreply.github.com>
2023-03-12 01:53:05 +0800

    Tugas1_SourceCode_Praktek

---
+++ Asgdll.java
@@ -0,0 +1,16 @@
+/*NIM :13020210088
+  Nama:Riany Rezeqy Utamie
+  Hari/Tanggal : Kamis, 9 Maret 2023
+  Waktu pengerjaan :10.00
+*/
+public class Asgdll{
+
+	public static void main(String[] args) {
+
+		float f= 20.0f;
+		double fll;
+
+		fll=10.0f;
+		System.out.println ("f : "+f+ "\nf11: "+fll);
+	}
+}
